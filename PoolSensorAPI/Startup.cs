da7bdea4f8ff7adb1324fe8a536a0ad697502ece
Mattias Lij <mattiaslij@gmail.com>
2021-04-25 15:26:34 +0200

    Fixed CORS issue and json serializes to Pascal case

--- PoolSensorAPI/Startup.cs
+++ PoolSensorAPI/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using System;
 
@@ -23,12 +22,19 @@ namespace PoolSensorAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
+
             var connString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
             services.AddSingleton<IMyAppSettings>(i => new MyAppSettings { AzureFileConnectionString = connString });
             services.AddSingleton<ITableStorage, AzureTableStorage>();
             services.AddTransient<IPoolSensorRepository, PoolSensorRepository>();
 
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(o => o.JsonSerializerOptions.PropertyNamingPolicy = null);
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PoolSensorAPI", Version = "v1" });
@@ -38,6 +44,7 @@ namespace PoolSensorAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin());
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PoolSensorAPI v1"));
